7e20067432afa44ac7c94d3e4031607df815a84e
Eren Çakır <eren.ckkr03@gmail.com>
2026-02-17 10:50:21 +0300

    linked lists have been created

---
+++ src/ft_lstclear.c
@@ -0,0 +1,3 @@
+#include <libft.h>
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{}
--- src/libft.h
+++ src/libft.h
@@ -16,4 +16,10 @@ void *ft_memcpy(void *dest, const void *src, size_t n);
 
 size_t ft_strlen(const char *s);
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 #endif // LIBFT_H
